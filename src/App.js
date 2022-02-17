c69ffc2af0d5778a2420265d437d5f4b09c8d810
Irfan Kurniawan <irfan@unity.sg>
2022-02-17 09:54:32 +0700

    publish

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+	const responseFacebook = (response) => {
+		console.log(response.email);
+	}
+  const responseGoogle = (response) => {
+    console.log(response.Ju.zv);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login With FB and GG
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <FacebookLogin
+            appId={process.env.REACT_APP_FB}
+            fields="name,email,picture"
+            callback={responseFacebook}
+            icon="fa-facebook"
+          />
+          <br />
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GG}
+            buttonText="LOGIN WITH GOOGLE"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+          />
       </header>
     </div>
   );
